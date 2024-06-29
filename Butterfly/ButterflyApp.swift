4aa6b524492257d602373ff6f5245268797cb4eb
NeVajnoKak <156511620+NeVajnoKak@users.noreply.github.com>
2024-06-29 23:08:43 +0500

    Initial Commit

---
+++ Butterfly/ButterflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButterflyApp.swift
+//  Butterfly
+//
+//  Created by Erkebulan Massainov on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ButterflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
